ec523b9ff33ae0f8c265fb876661ce63bc595bbf
fangmai001 <fangmai001@gmail.com>
2023-09-20 23:27:29 +0800

    fix: all page

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,57 @@
 import streamlit as st
 import pandas as pd
+from datetime import datetime
+from datetime import  timedelta
 from app.main.Main import Main
 
+# ==================== 定義預設值 ====================
 
-text_select = st.selectbox(
+today = datetime.today()
+default_end_date = today.strftime('%Y-%m-%d')
+default_start_date = (today - timedelta(days=3*365)).strftime('%Y-%m-%d')
+is_fund = False
+using_taiwan_stock_crawl = False
+using_taiwan_etf_crawl = False
+using_yf = False
+consider_tax = False
+
+# ==================== 標題 ====================
+
+st.title('股票定期定額模擬器💵')
+st.caption('股票定期定額回測的模擬器。')
+st.caption('*~~早知當初... 都有定期定額買，現在會怎麼樣🥲~~*')
+
+# ==================== 基本設定 ====================
+
+st.subheader('基本設定', divider=True)
+mode_str = st.radio(
+    "選擇類型",
+    [
+        "台股",
+        '自行填寫代號(台股)',
+        "美股",
+        '自行填寫代號(美股)',
+        '~~台灣基金 (施工中🚧)~~',
+        '~~VWRA (施工中🚧)~~',
+    ])
+start_date = st.text_input("開始時間", default_start_date)
+end_date = st.text_input("結束時間", default_end_date)
+
+investment_amount = st.number_input('每次定期定額投資金額', value=10000)
+purchase_day = st.number_input('每月第 N 個交易天進行交易', value=1)
+
+# is_fund = st.checkbox('是否為基金？', False)
+# using_taiwan_stock_crawl = st.checkbox('使用台股爬蟲(goodinfo.tw)？', True)
+# using_taiwan_etf_crawl = st.checkbox('使用台股ETF爬蟲(www.moneydj.com)？', False)
+# using_yf = st.checkbox('使用 Yahoo 股市API(使用美股需開啟)？', False)
+# consider_tax = st.checkbox('考量到美股股息稅(使用美股需開啟)？', False)
+
+# ==================== 選擇股票 ====================
+
+st.subheader('選擇股票', divider=True)
+
+if mode_str == '台股':
+    code = st.selectbox(
         '選擇代號',
         (
             '0050',
@@ -17,31 +65,59 @@ text_select = st.selectbox(
             '9941-裕融',
             '2480-敦陽科',
             '2618-長榮航空',
-            'VWRA.L',
-            'VT',
+        )
+    )
+    code = code.split('-')[0]
+    code = code + '.TW'
+    using_taiwan_stock_crawl = True
+    using_taiwan_etf_crawl = False
+    # using_taiwan_stock_crawl = st.checkbox('使用台股爬蟲(goodinfo.tw)？', True)
+    # using_taiwan_etf_crawl = st.checkbox('使用台股ETF爬蟲(www.moneydj.com)？', False)
+
+if mode_str == '自行填寫代號(台股)':
+    code = st.text_input("自行填寫代號(台股)")
+    code = code + '.TW'
+    using_taiwan_stock_crawl = True
+    using_taiwan_etf_crawl = False
+    # using_taiwan_stock_crawl = st.checkbox('使用台股爬蟲(goodinfo.tw)？', True)
+    # using_taiwan_etf_crawl = st.checkbox('使用台股ETF爬蟲(www.moneydj.com)？', False)
+
+if mode_str == '台灣基金':
+    code = st.selectbox(
+        '選擇代號',
+        (
             'ACDD01-安聯台灣大壩基金A累積型(台幣)'
         )
     )
-text_input = st.text_input("找不到？試試自行填寫代號")
-is_tw_stock = st.checkbox('是否為台股？', True)
-start_date = st.text_input("開始時間", "2022-01-01")
-end_date = st.text_input("結束時間", "2023-06-18")
-investment_amount = st.number_input('每次定期定額投資金額', value=10000)
-purchase_day = st.number_input('每月第 N 個交易天進行交易', value=1)
-is_fund = st.checkbox('是否為基金？', False)
-using_taiwan_stock_crawl = st.checkbox('使用台股爬蟲(goodinfo.tw)？', True)
-using_taiwan_etf_crawl = st.checkbox('使用台股ETF爬蟲(www.moneydj.com)？', False)
-using_yf = st.checkbox('使用 Yahoo 股市API(使用美股需開啟)？', False)
-consider_tax = st.checkbox('考量到美股股息稅(使用美股需開啟)？', False)
-
-if st.button('開始試算'):
-    if text_input:
-        code = text_input
-    if text_select:
-        code = text_select
     code = code.split('-')[0]
-    if is_tw_stock:
-        code = code + '.TW'
+    is_fund = True
+
+if mode_str == 'VWRA':
+    code = 'VWRA.L'
+    using_yf = True
+
+if mode_str == '美股':
+    code = st.selectbox(
+        '選擇代號',
+        (
+            'VT',
+            'VTI',
+            'SPY',
+            'AAPL',
+            'NVDA',
+            'TSLA',
+        )
+    )
+    using_yf = True
+    consider_tax = st.checkbox('考量到美股股息稅(30%)？', True)
+
+if mode_str == '自行填寫代號(美股)':
+    code = st.text_input("自行填寫代號(美股)")
+    using_yf = True
+    consider_tax = st.checkbox('考量到美股股息稅(30%)？', True)
+
+# ==================== 開始試算 ====================
+if st.button('開始試算'):
     st.write("使用代號: ", code)
 
     app = Main()
